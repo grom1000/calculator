6ab2f1a791741aa7ac5a087d0dddb8a55b4fd7ea
Nikolay <ntamarin27@gmail.com>
2020-06-16 10:52:51 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,45 +1,119 @@
 window.onload = function() {
+
     var display = document.querySelector('.display');
     var displayTotal = document.querySelector('.displayTotal');
+    var numberSelect = '';
+    var numberSelectCount = 0;
+    var numberSelectTotal = '';
+    var erase = false;
+    var sum = 0;
     var count = 0;
-    var sign = document.querySelectorAll('#sign');
-    var number = document.querySelectorAll('#number');
-    var equally = document.querySelector('#equally');
-    var arr = ['*', '-', '+', '='];
-    var a = null;
-    var b = null;
-        number.forEach(function(element) {
-            element.addEventListener('click', function() {
-                        display.textContent = this.innerHTML;
-                        count = Number(this.innerHTML);
-                })
-                
-        })
-        sign.forEach(function(element) {
-            element.addEventListener('click', function() {
-                        displayTotal.textContent = display.textContent + ' ' + this.innerHTML;
-                })
-                
-        })
-        function equallyTotal() {
-            displayTotal.textContent = displayTotal.textContent + ' ' + display.textContent + ' ' + '=';
-            display.textContent = Number('displayTotal.textContent');
-        }
-    
-    equally.onclick = equallyTotal;
-    console.log(count);
-    
-    
-    // for (var i = 0; i < item.length; i++) {
-    //     var element = item[i]
-    //     console.log(element);
-    //     element.addEventListener('click', function() {
-    //         if (this.innerHTML == '-') {
-    //             display.textContent = display.textContent
-    //         } else {
-    //             display.textContent = this.innerHTML;
-    //         }
+    var sign;
+    var eq = false;
+
+    display.textContent = '0';
+    displayTotal.textContent = '';
+
+    // function number() {
+    //     display.textContent = 0;
+    //     number.addEventListener('click', function() {
+
     //     })
     // }
-}
 
+    one.addEventListener('click', function() {
+        display.textContent = one.textContent;
+        numberSelect = 1;
+    })
+    two.addEventListener('click', function() {
+        display.textContent = two.textContent;
+    })
+    three.addEventListener('click', function() {
+        display.textContent = three.textContent;
+    })
+    four.addEventListener('click', function() {
+        display.textContent = four.textContent;
+    })
+    five.addEventListener('click', function() {
+        display.textContent = five.textContent;
+    })
+    six.addEventListener('click', function() {
+        display.textContent = six.textContent;
+    })
+    seven.addEventListener('click', function() {
+        display.textContent = seven.textContent;
+    })
+    eight.addEventListener('click', function() {
+        display.textContent = eight.textContent;
+    })
+    nine.addEventListener('click', function() {
+        if (erase) {
+            displayTotal.textContent = sum + ' +';
+            numberSelect = '9';
+            numberSelectTotal += numberSelect;  // строка
+            numberSelectCount = +(numberSelectTotal); // число
+            display.textContent = numberSelectTotal;
+        } else {
+            // displayTotal.textContent = numberSelect + ' +';
+            numberSelect = '9';
+            numberSelectTotal += numberSelect;  // строка
+            numberSelectCount = +(numberSelectTotal); // число
+            display.textContent = numberSelectTotal;
+        }
+    })
+    plus.addEventListener('click', function() {
+        if (erase) {
+            displayTotal.textContent = display.textContent + ' + ';
+            // display.textContent = display.textContent;
+        } else {
+        sign = 'plus';
+        // count = true;
+        sum += numberSelectCount;
+        displayTotal.textContent += numberSelectTotal + ' + ';
+        numberSelectTotal = '';
+        display.textContent = sum;
+        }
+    })
+    minus.addEventListener('click', function() {
+        sign = 'minus';
+        count = numberSelectCount;
+        sum -= numberSelectCount;
+        displayTotal.textContent += display.textContent + ' - ';
+        numberSelectTotal = '';
+        display.textContent = sum;
+    })
+    equally.addEventListener('click', function() {
+        eq = true;
+        // count = true;
+        erase = true;
+        switch(sign) {
+            case 'plus':
+                displayTotal.textContent += numberSelectCount + ' = ';
+                numberSelectTotal = '';
+                sum += numberSelectCount;
+                display.textContent = sum;
+                    // displayTotal.textContent = sum + ' + ' + numberSelectCount + ' = ';
+                    // numberSelectTotal = '';
+                    // sum += numberSelectCount;
+                    // display.textContent = sum;
+                break;
+            case 'minus':
+                displayTotal.textContent -= numberSelectCount + ' = ';
+                numberSelectTotal = '';
+                sum -= numberSelectCount;
+                display.textContent = sum;
+                    // displayTotal.textContent = sum + ' + ' + numberSelectCount + ' = ';
+                    // numberSelectTotal = '';
+                    // sum += numberSelectCount;
+                    // display.textContent = sum;
+                break;
+        }
+        
+    })
+    // erase.addEventListener('click', function() {
+    //     display.textContent = 0;
+    //     displayTotal.textContent = '';
+    //     numberTotal = 0;
+    // })
+    
+}
